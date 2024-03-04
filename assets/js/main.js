432037958cec4c6c481495ac33547423685c1ac7
ValentinCB <valentin.bouchentouf@gmail.com>
2024-03-04 23:51:42 +0100

    layout main.js

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Grab elements
+const selectElement = selector => {
+    
+}
+
+//Nav styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close search form popup
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
