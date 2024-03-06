50f8259e637e8c00d869c74450b858181052b1bf
ValentinCB <valentin.bouchentouf@gmail.com>
2024-03-06 15:06:22 +0100

    add swiper-bundle.min.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 // Grab elements
 const selectElement = selector => {
-    
-}
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly`);
+
+};
+console.log(selectElement('.navbar'));
 
 //Nav styles on scroll
 
